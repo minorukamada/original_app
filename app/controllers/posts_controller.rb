564f8106514983d809867546ef1d945445e27166
minoru.kamada <minbooo185@gmail.com>
2019-06-16 15:02:48 +0000

    delete button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :require_user_logged_in
+  # before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
 
   def create
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,8 +1,8 @@
 class ToppagesController < ApplicationController
   def index
-    if logged_in?
-      @post = current_user.posts.build  # form_with 用
-      @posts = current_user.posts.order(id: :desc).page(params[:page])
-    end
+    # if logged_in?
+      @posts = Post.order(id: :desc).page(params[:page]).per(25)
+      @users = User.order(id: :desc).page(params[:page]).per(25)
+    # end
   end
 end
