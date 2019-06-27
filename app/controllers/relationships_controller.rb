98f704f109366f46f8b83a1a17b1f69dbf6b7e11
minoru.kamada <minbooo185@gmail.com>
2019-06-27 14:34:03 +0000

    ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,20 @@ class RelationshipsController < ApplicationController
   before_action :require_user_logged_in
 
   def create
-    user = User.find(params[:follow_id])
-    current_user.follow(user)
-    flash[:success] = 'ユーザをフォローしました。'
-    redirect_back(fallback_location: root_url)
+    
+    @user = User.find(params[:follow_id])
+    @follow_id = params[:follow_id]
+    current_user.follow(@user)
+    # flash[:success] = 'ユーザをフォローしました。'
+    # redirect_back(fallback_location: root_url)
   end
 
   def destroy
-    user = User.find(params[:follow_id])
-    current_user.unfollow(user)
-    flash[:success] = 'ユーザのフォローを解除しました。'
-    redirect_back(fallback_location: root_url)
+    
+    @user = User.find(params[:follow_id])
+    @follow_id = params[:follow_id]
+    current_user.unfollow(@user)
+    # flash[:success] = 'ユーザのフォローを解除しました。'
+    # redirect_back(fallback_location: root_url)
   end
 end
