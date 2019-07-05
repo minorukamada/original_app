43f28830be60e0217090549c74ef5bb8395eae3a
minoru.kamada <minbooo185@gmail.com>
2019-07-05 09:47:49 +0000

    follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,15 +5,15 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:follow_id])
     @follow_id = params[:follow_id]
     current_user.follow(@user)
-    # flash[:success] = 'ユーザをフォローしました。'
-    # redirect_back(fallback_location: root_url)
+    flash[:success] = 'ユーザをフォローしました。'
+    redirect_back(fallback_location: root_url)
   end
 
   def destroy
     @user = User.find(params[:follow_id])
     @follow_id = params[:follow_id]
     current_user.unfollow(@user)
-    # flash[:success] = 'ユーザのフォローを解除しました。'
-    # redirect_back(fallback_location: root_url)
+    flash[:success] = 'ユーザのフォローを解除しました。'
+    redirect_back(fallback_location: root_url)
   end
 end
