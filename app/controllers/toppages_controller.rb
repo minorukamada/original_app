e670e0ab5c81cb320f59756f831b8869cfcd6946
minoru.kamada <minbooo185@gmail.com>
2019-07-14 09:01:01 +0000

    for heroku7

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,6 @@
 class ToppagesController < ApplicationController
   def index
-    @posts_header = Post.includes(:user).order(id: :desc).page(params[:post_page]).per(4)
+    @posts_header = Post.includes(:user).order(id: :desc).limit(4)
     @posts = Post.includes(:user).order(id: :desc).page(params[:post_page]).per(10)
     @users = User.includes(:posts).order(id: :desc).page(params[:user_page]).per(5)
   end
