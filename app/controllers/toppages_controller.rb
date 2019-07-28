fd576812fe9b8140e019ec9d407a3c6f42c57e90
minoru.kamada <minbooo185@gmail.com>
2019-07-28 14:59:16 +0000

    for heroku12

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     @posts_header = Post.includes(:user).order(id: :desc).limit(4)
-    @posts = Post.includes(:user).order(id: :desc).page(params[:post_page]).per(10)
     @users = User.includes(:posts).order(id: :desc).page(params[:user_page]).per(5)
+    @posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post_id))
   end
 end
